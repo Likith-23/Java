4bd70737c5b993424dbfd0b481417f3dd490b542
Likith-23 <likithsairajpignos@gmail.com>
2025-07-04 21:05:20 +0200

    initial

---
+++ hw1.java
@@ -0,0 +1,8 @@
+public class hw1 {
+    public static void main(String[] args){
+        int x = 5;
+        int y = 6;
+        int sum = x + y;
+        System.out.println("The sum of 5 and 6 is..."+sum);
+    }
+}
