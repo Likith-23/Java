6cd4d1ec864875e18fcd4d5fb3d997edb28c95b0
Likith-23 <likithsairajpignos@gmail.com>
2025-07-14 09:58:25 +0200

    intial

---
+++ HW2/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        String first="Likith's ";
+        String second = "Report Card";
+        String report = first+second;
+        int math = 90;
+        int science = 85;
+        int english = 100;
+        System.out.println(report);
+        System.out.println("MATH---"+math);
+        System.out.println("Science---"+science);
+        System.out.println("English---"+english);
+        System.out.println("AVERAGE ---" );
+    }
+}
